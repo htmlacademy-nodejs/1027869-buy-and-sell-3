067859cbfc1f6cf78c5f82636eb7e4c44b53f5d8
sergey talyzin <sergeytalyzin@MacBook-Pro-sergey.local>
2020-11-09 16:34:03 +0300

    finish m2-t1

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -1,7 +1,8 @@
 'use strict';
 const {getRandomInt, shuffle, getPictureFileName} = require(`../../utils`);
 const {CATEGORIES, SENTENCES, TITLES, OfferType, SumRestrict, PictureRestrict, DEFAULT_COUNT, FILE_NAME} = require(`../../constants`);
-const fs = require(`fs`);
+const fs = require(`fs`).promises;
+const chalk = require('chalk')
 
 
 const generateOffers = (count) => (
@@ -17,16 +18,15 @@ const generateOffers = (count) => (
 
 module.exports = {
   name: `--generate`,
-  run(args) {
+ async run(args) {
     const [count] = args;
     const countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT;
     const content = JSON.stringify(generateOffers(countOffer));
-
-    fs.writeFile(FILE_NAME, content, (err) => {
-      if (err) {
-        return console.error(`Can't write data to file...`);
-      }
-      return console.info(`Operation success. File created.`);
-    });
+   try {
+     await fs.writeFile(FILE_NAME, content);
+     console.log(chalk.green(`Operation success. File created.`));
+   } catch (err) {
+     console.error(chalk.red(`Can't write data to file...`));
+   }
   }
 };
