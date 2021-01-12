2136d51aa97ebb6381c0347dcd671b0c4b4f39f4
Sergey <st@ttsgroup.su>
2021-01-12 19:26:25 +0300

    m4-t3

--- src/express/index.js
+++ src/express/index.js
@@ -6,17 +6,21 @@ const offersRoutes = require(`./routes/offers-routes`);
 const myRoutes = require(`./routes/my-routes`);
 const mainRoutes = require(`./routes/main-routes`);
 const DEFAULT_PORT = 8081;
+const PUBLIC_DIR = `public`;
+const UPLOAD_DIR = `upload`;
+
 const app = express();
 
 app.use(`/offers`, offersRoutes);
 app.use(`/my`, myRoutes);
 app.use(`/`, mainRoutes);
 
-const PUBLIC_DIR = `public`;
+
 app.set(`views`, path.resolve(__dirname, `templates`));
 app.set(`view engine`, `pug`);
 
 app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+app.use(express.static(path.resolve(__dirname, UPLOAD_DIR)));
 app.use((req, res) => res.status(404).render(`errors/404`));
 app.use((err, req, res, _next) => res.status(500).render(`errors/500`));
 
--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -1,12 +1,29 @@
 'use strict';
 const {Router} = require(`express`);
 const mainRoutes = new Router();
+const api = require(`../api`).getAPI();
 
-mainRoutes.get(`/search`, (req, res) => res.render(`search-result`));
+
 mainRoutes.get(`/register`, (req, res) => res.render(`sign-up`));
 mainRoutes.get(`/login`, (req, res) => res.render(`login`));
-mainRoutes.get(`/`, (req, res) => res.render(`main`));
+mainRoutes.get(`/`, async (req, res) => {
+  const offers = await api.getOffers();
+  res.render(`main`, {offers});
+});
 
+mainRoutes.get(`/search`, async (req, res) => {
+  try {
+    const {search} = req.query;
+    const results = await api.search(search);
+    res.render(`search-result`, {
+      results
+    });
+  } catch (error) {
+    res.render(`search-result`, {
+      results: []
+    });
+  }
+});
 
 module.exports = mainRoutes;
 
